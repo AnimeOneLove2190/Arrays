5f64e7a06d6d9ae85f3333b17dc6e2a372bfbf94
animeonelove2190 <daniil.davletshin1@gmail.com>
2023-08-04 05:25:23 +0500

    Добавьте файлы проекта.

---
+++ Project01/ArrayService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project01
+{
+    class ArrayService
+    {
+        public int[] GetRandomArray(int arraySize, int minValue, int maxValue)
+        {
+            int[] randomArray = new int[arraySize];
+            Random godOfNumbers = new Random();
+            for (int i = 0; i < randomArray.Length; i++)
+            {
+                randomArray[i] = godOfNumbers.Next(minValue, maxValue);
+            }
+            return randomArray;
+        }
+        public void WriteArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
---
+++ Project01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Project01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayService service = new ArrayService();
+            Random godOfNumbers = new Random();
+            int arraySize = int.Parse(Console.ReadLine());
+            int minValue = int.Parse(Console.ReadLine());
+            int maxValue = int.Parse(Console.ReadLine());
+            int[] givenArray = service.GetRandomArray(arraySize, minValue, maxValue);
+            service.WriteArray(givenArray);
+        }
+    }
+}
